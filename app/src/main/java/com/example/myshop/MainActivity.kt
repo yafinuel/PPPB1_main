bf14f23dd6be8036e3cde0d50edb1ce4cfda7e5c
yafinuel <yafinuqmanelianto@gmail.com>
2025-09-09 15:59:54 +0700

    pertemuan 4

---
+++ app/src/main/java/com/example/myshop/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myshop
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myshop.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navController)
+        }
+
+    }
+}
